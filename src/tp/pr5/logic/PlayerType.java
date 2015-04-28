7ecda4c2ea857cd137b018a403cf1a2e7c936e43
Fran <franloza@ucm.es>
2015-04-28 11:33:36 +0200

    Added PlayerType enum

---
+++ src/tp/pr5/logic/PlayerType.java
@@ -0,0 +1,25 @@
+package tp.pr5.logic;
+
+
+/**
+ * Represents the type of player, either Human or Automatic.
+ * The enumeration is used to store the information of what type of player is playing.
+ *
+ * @author: Alvaro Bermejo
+ * @author: Francisco Lozano
+ * @version: 28/04/2015
+ * @since: Assignment 5
+ */
+public enum PlayerType {
+	HUMAN("Human"), AUTO("Automatic");
+	
+	private String name;
+	
+	PlayerType(String name) {
+		this.name = name;
+	}
+	
+	public String toString() {
+		return name;
+	}
+}
