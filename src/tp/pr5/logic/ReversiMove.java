08dab8a0c090b41873be85705a4e9e900fcf3460
Mortadelegle <mortadelegle@gmail.com>
2015-04-28 10:40:25 +0200

    Converted Position into a nested class
    It is inside ReversiMove now

--- src/tp/pr5/logic/ReversiMove.java
+++ src/tp/pr5/logic/ReversiMove.java
@@ -109,7 +109,7 @@ public class ReversiMove extends Move {
 		//                │
 		//                │
 		//  Dir 7         │           Dir 3
-		// (-1,0) ────────│────────── (1,0)
+		// (-1,0) ────────┼────────── (1,0)
 		//                │
 		//                │
 		//  Dir 6         │          Dir 4
@@ -234,5 +234,43 @@ public class ReversiMove extends Move {
 		}
 	}
 
+	/**
+	 * Class that facilitates the work with a position in the board
+	 *
+	 * @author: Alvaro Bermejo
+	 * @author: Francisco Lozano
+	 * @version: 25/04/2015
+	 * @since: Assignment 5
+	 */
+	private class Position {
+		//Attributes
+		private int posX;
+		private int posY;
 
+		//Functions
+
+		//Getters
+		public int getPosX() {
+			return posX;
+		}
+
+		public int getPosY() {
+			return posY;
+		}
+
+		//Setters
+		public void setPosX(int posX) {
+			this.posX = posX;
+		}
+
+		public void setPosY(int posY) {
+			this.posY = posY;
+		}
+
+		public void setPos(int posX, int posY) {
+			this.posX = posX;
+			this.posY = posY;
+		}
+
+	}
 }
